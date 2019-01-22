58ba0a37ac01d515c1065f1d2bb08b12bd89a1a8
seifboubaker <boubaker.seif@gmail.com>
2019-01-22 19:43:34 +0100

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from './post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'OCBlog';
+export class AppComponent implements OnInit {
+  private postList: Post[];
+
+  ngOnInit(): void {
+    this.postList = [{
+        title: 'Mon Premier post',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        loveIts: 0,
+        created_at: new Date()
+      },
+      {
+        title: 'Mon Deuxième post',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        loveIts: 0,
+        created_at: new Date()
+      },
+      {
+        title: 'Mon Troisième post',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        loveIts: 0,
+        created_at: new Date()
+    }];
+  }
+
+  get postsList() {
+    return this.postList;
+  }
 }
---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../post';
+
+@Component({
+  selector: 'app-post-list-item-component',
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.css']
+})
+export class PostListItemComponentComponent implements OnInit {
+  @Input() post: Post;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loveItIncr() {
+    this.post.loveIts++;
+  }
+
+  loveItDecr() {
+    this.post.loveIts--;
+  }
+}
