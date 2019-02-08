eff1c3b0b4d00ba9b1719f81fdd8d1fc8a834e16
seifboubaker <boubaker.seif@gmail.com>
2019-02-08 17:00:10 +0100

    Réorganisation des fichiers, ajout do routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponentComponent } from './post-list-component/post-list-component.component';
 import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
+import { HeaderComponent } from './header/header.component';
+import {Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'posts', component: PostListComponentComponent },
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'posts' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponentComponent,
-    PostListItemComponentComponent
+    PostListItemComponentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
